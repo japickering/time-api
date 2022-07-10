847ba22341e49386f7a7961bd190545908846ed7
James Pickering <james.pickering@gmail.com>
2022-07-10 15:34:59 +0100

    create express server config

---
+++ README.md
@@ -0,0 +1,26 @@
+# Server Time API Specification
+
+Create a Node Express server that serves an api endpoint /time that accepts a GET request and
+returns a JSON response that includes the current server time, in epoch seconds, at the time of processing the request.
+
+Responses returned from the API should validate against the following JSON schema:
+
+## GET /time
+```
+{
+ "properties": {
+ "epoch": {
+ "description": "The current server time, in epoch
+seconds, at time of processing the request.",
+ "type": "number"
+ }
+ },
+ "required": ["epoch"],
+ "type": "object"
+}
+```
+
+The API should return a 403 response code for all API requests that do not include header Authorization with value ‘mysecrettoken’.
+
+## GET /metrics
+Response should serve all available Prometheus-format metrics for the API, including default recommended metrics (see configuration value collectDefaultMetrics).
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const promMid = require('express-prometheus-middleware');
+const app = express();
+const server = require('http').createServer(app);
+// const cors = require('cors');
+
+const PORT = 4000;
+
+// More info: https://www.npmjs.com/package/express-prometheus-middleware
+app.use(
+  promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+  })
+);
+
+// curl -X GET localhost:4000/hello?name=Chuck%20Norris
+app.get('/hello', (req, res) => {
+  console.log('GET /hello');
+  const { name = 'Anon' } = req.query;
+  res.json({ message: `Hello, ${name}!` });
+});
+
+// curl -X GET localhost:4000/time
+app.get('/time', function (req, res) {
+  res.send(
+    JSON.stringify({
+      time: Date.now(),
+    })
+  );
+});
+
+// curl -X GET localhost:4000/metrics
+app.get('/metrics', function (req, res) {
+  res.send(JSON.stringify({}));
+});
+
+server.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
